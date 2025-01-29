28ddaeabe7a1cff67a376d8239fe056942cf9d28
pmagicguy <402919@vistacollege.nl>
2025-01-29 10:00:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -49,10 +49,10 @@ echo "</div>";
 <div>
     <h1>Racetimes</h1>
     <button id="btn">
-        <a href="../Racing-Times/html/signup.html">Sign up!</a>
+        <a href="html/signup.html">Sign up!</a>
     </button>
     <button>
-        <a href="../Racing-Times/html/login.html">Login</a>
+        <a href="html/login.html">Login</a>
     </button>
     <form action="index.php" method="POST">
         <label for="name">Name:</label>
